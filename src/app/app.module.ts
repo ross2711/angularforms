8308ac568336ebe3cb6da300c8a648d3a59acc76
ross <ross.fleming@serviceware.se>
2019-06-11 09:06:20 +0200

    form components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+import { TemplateformsComponent } from './templateforms/templateforms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveformsComponent,
+    TemplateformsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/templateforms/templateforms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templateforms',
+  templateUrl: './templateforms.component.html',
+  styleUrls: ['./templateforms.component.scss']
+})
+export class TemplateformsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
